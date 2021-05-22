5984ddaf611d42eb98eb2921b7c507cd01c02d12
JaviMontoto <javi@javimontoto.com>
2021-05-22 20:03:13 +0200

    Quitada la parte de localStorage para las tareas (solo se conserva el usuario). Las tareas se puede ordenar. Botón para resetar la contraseña.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,25 @@
+/*** VARIABLES GLOBALES ***/
+/** Array con las tareas */
+let tareas = {};
+/** Última posición ocupada */
+let lastPosition = 0;
+/** Objeto con el usuario usuario de FB */
+let user = {}; //=> Usuario de firebase 
+
+
 /*** SELECTORES ***/
 const formulario = document.getElementById('formulario'),
     input = document.getElementById('input'),
     listaTareas = document.getElementById('lista-tareas'),
     template = document.getElementById('template').content,
-    fragment = document.createDocumentFragment();
-
-
-/*** VARIABLES ***/
-let tareas = {}; //=> Contiene las tareas 
-let user = {}; //=> Usuario de firebase 
+    fragment = document.createDocumentFragment(),
+    loginModalButton = document.getElementById('login-modal-button'),
+    registerModalButton = document.getElementById('register-modal-button'),
+    loginButton = document.getElementById('login-button'),
+    registerButton = document.getElementById('register-button'),
+    verificationButton = document.getElementById('verification-button'),
+    logoutButton = document.getElementById('logout-button'),
+    resetPassButton = document.getElementById('reset-pass-button');
 
 
 /*** LISTENERS ***/
@@ -19,40 +30,39 @@ document.addEventListener('DOMContentLoaded', () => {
     getAllTasks();
 });
 
-listaTareas.addEventListener('click', e => {
-    btnAccion(e);
-})
+listaTareas.addEventListener('click', e => { btnAccion(e); });
+formulario.addEventListener('submit', e => { addTarea(e); });
+loginButton.addEventListener('click', singInFB, false);
+registerButton.addEventListener('click', signUpFB, false);
+verificationButton.addEventListener('click', sendEmailVerification, false);
+logoutButton.addEventListener('click', signOutFB, false);
+resetPassButton.addEventListener('click', sendPasswordReset, false);
 
-formulario.addEventListener('submit', e => {
-    e.preventDefault();
-    setTarea(e);
-});
-
-
-/*** FUNCIONES ***/
-
-async function getAllTasks() {
+/*** FUNCIONES PRINCIPALES ***/
+/**
+ * Carga la lista de tareas de FB si hay sesión o si no del localStorage
+ */
+function getAllTasks() {
     if (localStorage.getItem('user')) {
         // Tenemos usuario logueado => recuperamos tareas de FB
         user = JSON.parse(localStorage.getItem('user'));
         showCloseButton(true);
-        await getFBTasks();
+        showLoader(true);
+        getFBTasks(pintarTareas);
 
     } else {
         // No tenemos usuario logueado => Recuperamos las tareas guardas en localStorage
-        if (localStorage.getItem('tareas'))
-            tareas = JSON.parse(localStorage.getItem('tareas'));
         showCloseButton(false);
     }
-
-    pintarTareas();
 }
 
 /**
  * Guardar la tarea
- * @param {event} e 
+ * @param {Event} e 
  */
-function setTarea(e) {
+function addTarea(e) {
+    e.preventDefault();
+
     if (input.value.trim() === '')
         return;
 
@@ -61,10 +71,14 @@ function setTarea(e) {
     tarea.id = Date.now();
     tarea.title = input.value;
     tarea.status = 'active';
+    tarea.position = lastPosition + 1;
 
-    // Añadimos la tarea a FB
-    if (!saveTask(tarea))
-        return;
+    // Comprobamos si estamos logueados
+    if (localStorage.getItem('user')) {
+        // Añadimos la tarea a FB
+        if (!saveTask(tarea))
+            return;
+    }
 
     // Añadimos a la colección
     tareas[tarea.id] = tarea;
@@ -80,8 +94,8 @@ function setTarea(e) {
  */
 function pintarTareas() {
 
-    // Guardamos en localStorage
-    localStorage.setItem('tareas', JSON.stringify(tareas));
+    // Ocultamos el loader
+    showLoader(false);
 
     // Comprobamos si la lista de objetos está vacía
     if (Object.values(tareas).length === 0) {
@@ -89,11 +103,15 @@ function pintarTareas() {
         return;
     }
 
+    // Ordenamos las tareas según su posición
+    sortTasks();
+
     // Limpiamos la lista de tareas
     listaTareas.innerHTML = '';
 
     Object.values(tareas).forEach(tarea => {
         const clone = template.cloneNode(true);
+        clone.querySelector('li').dataset.id = tarea.id;
         clone.querySelector('p').textContent = tarea.title;
 
         // Comprobamos si la tarea tiene el status realizada
@@ -117,11 +135,25 @@ function pintarTareas() {
     });
 
     listaTareas.appendChild(fragment);
+
+    // Hacemos la lista de tareas ordenable utilizando la función de Bootstrap
+    Sortable.create(listaTareas, {
+        onEnd: e => {
+            listaTareas.querySelectorAll('li').forEach((el, index) => {
+                tareas[el.dataset.id].position = index + 1;
+            });
+
+            // Actualizamos las tareas en FB
+            // TODO: Crear función que guarde todas las tareas a la vez
+            Object.values(tareas).forEach(tarea => saveTask(tarea));
+        }
+    });
+
 }
 
 /**
  * Acciones de los botones de tarea
- * @param {event} e 
+ * @param {Event} e 
  */
 function btnAccion(e) {
 
@@ -146,4 +178,135 @@ function btnAccion(e) {
     pintarTareas();
 
     e.stopPropagation();
+}
+
+
+
+/*** FUNCIONES AUXILIARES ***/
+/**
+ * Muestra u oculta el botón de cerrar sesión, y a la vez muestra u oculta los botones de entrar y registrarse
+ * @param {boolean} show True = muestra, False = oculta
+ */
+function showCloseButton(show) {
+    if (show) {
+        // Mostramos el botón de salir y ocultamos los de entrar y registrarse
+        loginModalButton.classList.replace('active', 'hidden');
+        registerModalButton.classList.replace('active', 'hidden');
+        logoutButton.classList.replace('hidden', 'active');
+
+        // Si no ha verificado el email mostramos el botón de reenvío
+        if (user.emailVerified) {
+            formulario.classList.replace('hidden', 'active');
+            listaTareas.classList.replace('hidden', 'active');
+        } else {
+            verificationButton.classList.replace('hidden', 'active');
+        }
+
+    } else {
+        // Ocultamos el botón de salir y mostramos los de entrar y registrarse
+        logoutButton.classList.replace('active', 'hidden');
+        verificationButton.classList.replace('active', 'hidden');
+        formulario.classList.replace('active', 'hidden');
+        listaTareas.classList.replace('active', 'hidden');
+        loginModalButton.classList.replace('hidden', 'active');
+        registerModalButton.classList.replace('hidden', 'active');
+
+    }
+}
+
+/**
+ * Cierra la modal con el id proporcionado
+ * @param {string} modalId Id de la modal: login-modal, register-modal
+ */
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.remove('show');
+    modal.setAttribute('aria-hidden', 'true');
+    modal.setAttribute('style', 'display: none');
+
+    // get modal backdrops
+    const modalsBackdrops = document.getElementsByClassName('modal-backdrop');
+
+    // remove every modal backdrop
+    for (let i = 0; i < modalsBackdrops.length; i++) {
+        document.body.removeChild(modalsBackdrops[i]);
+    }
+}
+
+/**
+ * Comprueba si el email y la contraseña tienen el formato adecuado y si no marca en rojo el campo
+ * @param {String} email 
+ * @param {String} pass 
+ * @param {String} type Acción que lo ejecuta: login, register o resetPass
+ * @returns {Boolean}
+ */
+function checkEmailAndPass(email, pass, type) {
+    let result = true,
+        resetPass = false;
+    const mailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    // Si la acción es resetear contraseña 
+    if (type === 'resetPass') {
+        type = 'login';
+        resetPass = true;
+    }
+
+    // Comprobamos el email
+    const emailInput = document.getElementById('email-' + type);
+    if (email.length === 0 || !mailFormat.test(String(email).toLowerCase())) {
+        emailInput.classList.add('is-invalid');
+        emailInput.nextElementSibling.classList.replace('hidden', 'active');
+        result = false;
+    } else {
+        emailInput.classList.remove('is-invalid');
+        emailInput.nextElementSibling.classList.replace('active', 'hidden');
+    }
+
+    if (!resetPass) {
+        // Comprobamos la contraseña
+        const passInput = document.getElementById('pass-' + type);
+        if (pass.length < 6) {
+            passInput.classList.add('is-invalid');
+            passInput.nextElementSibling.classList.replace('hidden', 'active');
+            result = false;
+        } else {
+            passInput.classList.remove('is-invalid');
+            passInput.nextElementSibling.classList.replace('active', 'hidden');
+        }
+    }
+
+    return result;
+}
+
+/**
+ * Muestra u oculta el gif de cargando
+ * @param {Boolean} show Muestra (true) u oculta (false)
+ */
+function showLoader(show) {
+    if (show) {
+        const loader = document.createElement('div');
+        loader.id = 'loader';
+        loader.classList.add('center');
+        listaTareas.appendChild(loader);
+    } else if (document.getElementById('loader')) {
+        document.getElementById('loader').remove();
+    }
+}
+
+/**
+ * Ordena las tareas según el campo posición
+ */
+function sortTasks() {
+    let tareasArray = Object.values(tareas);
+    if (tareasArray.length > 1) {
+        tareasArray.sort((a, b) => {
+            return a.position - b.position;
+        });
+        lastPosition = tareasArray[tareasArray.length - 1].position;
+        tareas = {};
+        tareasArray.forEach(tarea => tareas[tarea.id] = tarea);
+
+    } else
+        lastPosition = 1;
+
 }
--- assets/js/fb.js
+++ assets/js/fb.js
@@ -18,22 +18,6 @@ function initFirebase() {
     firebase.analytics();
 }
 
-/*** SELECTORES ***/
-const loginModalButton = document.getElementById('login-modal-button'),
-    registerModalButton = document.getElementById('register-modal-button'),
-    loginButton = document.getElementById('login-button'),
-    registerButton = document.getElementById('register-button'),
-    verificationButton = document.getElementById('verification-button'),
-    logoutButton = document.getElementById('logout-button'),
-    noLogginWarning = document.querySelector('.no-loggin-warning');
-
-
-/*** LISTENERS ***/
-loginButton.addEventListener('click', singInFB, false);
-registerButton.addEventListener('click', signUpFB, false);
-verificationButton.addEventListener('click', sendEmailVerification, false);
-logoutButton.addEventListener('click', signOutFB, false);
-
 
 /*** AUTENTIFICACIÓN ***/
 /**
@@ -54,7 +38,7 @@ function singInFB() {
                 user = firebase.auth().currentUser;
                 localStorage.setItem('user', JSON.stringify(user));
                 showCloseButton(true);
-                // ...
+                getAllTasks();
             })
             .catch((error) => {
                 // Handle Errors here.
@@ -149,11 +133,19 @@ function sendEmailVerification() {
         });
 }
 
+/**
+ * Envía un email para resetear la contraseña
+ * @returns Boolean Devuelve true si todo fue bien, false en otro caso
+ */
 function sendPasswordReset() {
-    var email = document.getElementById('email').value;
+    let email = document.getElementById('email-login').value;
+
+    if (!checkEmailAndPass(email, '', 'resetPass'))
+        return;
+
     firebase.auth().sendPasswordResetEmail(email).then(function() {
         // Password Reset Email Sent!
-        myAlert('Atención', 'Password Reset Email Sent!');
+        myAlert('Reseteo de contraseña', 'Se ha enviado un correo electrónico con las instrucciones para resetear la contraseña.');
     }).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -167,89 +159,8 @@ function sendPasswordReset() {
     });
 }
 
-/**
- * Muestra u oculta el botón de cerrar sesión, y a la vez muestra u oculta los botones de entrar y registrarse
- * @param {boolean} show True = muestra, False = oculta
- */
-function showCloseButton(show) {
-    if (show) {
-        // Mostramos el botón de salir y ocultamos los de entrar y registrarse
-        loginModalButton.classList.replace('d-block', 'd-none');
-        registerModalButton.classList.replace('d-block', 'd-none');
-        logoutButton.classList.replace('d-none', 'd-block');
-        noLogginWarning.classList.replace('d-block', 'd-none');
 
-        // Si no ha verificado el email mostramos el botón de reenvío
-        if (!user.emailVerified) {
-            verificationButton.classList.replace('d-none', 'd-block');
-        }
-
-    } else {
-        // Ocultamos el botón de salir y mostramos los de entrar y registrarse
-        loginModalButton.classList.replace('d-none', 'd-block');
-        registerModalButton.classList.replace('d-none', 'd-block');
-        noLogginWarning.classList.replace('d-none', 'd-block');
-
-        logoutButton.classList.replace('d-block', 'd-none');
-        verificationButton.classList.replace('d-block', 'd-none');
-    }
-}
-
-/**
- * Cierra la modal con el id proporcionado
- * @param {string} modalId Id de la modal: login-modal, register-modal
- */
-function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.classList.remove('show');
-    modal.setAttribute('aria-hidden', 'true');
-    modal.setAttribute('style', 'display: none');
-
-    // get modal backdrops
-    const modalsBackdrops = document.getElementsByClassName('modal-backdrop');
-
-    // remove every modal backdrop
-    for (let i = 0; i < modalsBackdrops.length; i++) {
-        document.body.removeChild(modalsBackdrops[i]);
-    }
-}
-
-/**
- * Comprueba si el email y la contraseña tienen el formato adecuado y si no marca en rojo el campo
- * @param {String} email 
- * @param {String} pass 
- * @param {String} type Acción que lo ejecuta: login o register
- * @returns true / false
- */
-function checkEmailAndPass(email, pass, type) {
-    let result = true;
-    const mailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    // Comprobamos el email
-    const emailInput = document.getElementById('email-' + type);
-    if (email.length === 0 || !mailFormat.test(String(email).toLowerCase())) {
-        emailInput.classList.add('is-invalid');
-        emailInput.nextElementSibling.classList.replace('d-none', 'd-block');
-        result = false;
-    } else {
-        emailInput.classList.remove('is-invalid');
-        emailInput.nextElementSibling.classList.replace('d-block', 'd-none');
-    }
-
-    // Comprobamos la contraseña
-    const passInput = document.getElementById('pass-' + type);
-    if (pass.length < 6) {
-        passInput.classList.add('is-invalid');
-        passInput.nextElementSibling.classList.replace('d-none', 'd-block');
-        result = false;
-    } else {
-        passInput.classList.remove('is-invalid');
-        passInput.nextElementSibling.classList.replace('d-block', 'd-none');
-    }
-
-    return result;
-}
-
+/*** MÉTODOS FB */
 /**
  * Guarda la tarea en FB
  * @param {Object} task Objeto de tipo tarea con id, status y title
@@ -258,7 +169,8 @@ function saveTask(task) {
     firebase.database().ref('tareas/' + user.uid + '/' + task.id).set({
             id: task.id,
             title: task.title,
-            status: task.status
+            status: task.status,
+            position: task.position
         })
         .catch((error) => {
             myAlert('Atención', error.message);
@@ -270,19 +182,24 @@ function saveTask(task) {
 
 /**
  * Recupera las tareas almacenadas en FB
+ * @param {function} callback Función que se ejutará después de realizar la petición a FB si todo va bien
  */
-async function getFBTasks() {
+function getFBTasks(callback) {
     const db = firebase.database().ref();
-    await db.child('tareas').child(user.uid).get()
+    db.child('tareas').child(user.uid).get()
         .then((taskList) => {
             if (taskList.exists()) {
                 tareas = taskList.val();
             } else {
                 tareas = {};
+                lastPosition = 0;
             }
+            callback();
+
         }).catch((error) => {
             myAlert('Atención', error.message);
         });
+
 }
 
 /**
@@ -293,7 +210,8 @@ function updateTask(task) {
     firebase.database().ref('tareas/' + user.uid + '/' + task.id).update({
             id: task.id,
             title: task.title,
-            status: task.status
+            status: task.status,
+            position: task.position
         })
         .catch((error) => {
             myAlert('Atención', error.message);
@@ -301,6 +219,10 @@ function updateTask(task) {
         });
 }
 
+/**
+ * Elimina una tarea
+ * @param {Object} task Objeto de tipo tarea con id, status y title
+ */
 function removeTask(task) {
     firebase.database().ref('tareas/' + user.uid + '/' + task.id).remove()
         .catch((error) => {
